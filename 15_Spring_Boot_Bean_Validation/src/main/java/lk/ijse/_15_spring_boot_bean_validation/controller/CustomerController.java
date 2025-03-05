476e8b776e500b8792172f1f8ead97d3eaae5ca0
A.M. Supun Madhuranga <157113777+smadhuranga@users.noreply.github.com>
2025-03-05 10:27:12 +0530

    15 ......

--- 15_Spring_Boot_Bean_Validation/src/main/java/lk/ijse/_15_spring_boot_bean_validation/controller/CustomerController.java
+++ 15_Spring_Boot_Bean_Validation/src/main/java/lk/ijse/_15_spring_boot_bean_validation/controller/CustomerController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class CustomerController {
     @PostMapping("/save")
-    public ResponseEntity<ResponseDTO> createCustomer(@RequestBody CustomerDTO customerDTO){
+    public ResponseEntity<ResponseDTO> createCustomer(@RequestBody @V CustomerDTO customerDTO){
         ResponseDTO responseDTO = new ResponseDTO("User Created Successfully", HttpStatus.CREATED.value(),customerDTO);
 
         return new ResponseEntity(responseDTO,HttpStatus.CREATED);
